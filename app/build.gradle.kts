01acf3d78e710e9e8ee677af913c4a66a31cca95
Ali <m.ali.rezaeiii09@gmail.com>
2025-01-19 01:01:18 +0330

    Using versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation)
     implementation(libs.qraphql)
+    implementation(libs.logging.interceptor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -63,7 +64,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 }
 
 apollo {
--- app/src/main/java/com/example/qraphql/di/ApolloModule.kt
+++ app/src/main/java/com/example/qraphql/di/ApolloModule.kt
@@ -18,10 +18,8 @@ object ApolloModule {
     @Provides
     @Singleton
     fun getApolloClient(): ApolloClient {
-
         val loggingInterceptor = HttpLoggingInterceptor { message -> Log.d("Apollo", message) }
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY // Log full body
-
         return ApolloClient.Builder()
             .serverUrl("https://countries.trevorblades.com/graphql")
             .okHttpClient(
