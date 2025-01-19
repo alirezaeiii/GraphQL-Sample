1ee63ea6dc1c03db6a0c60d5b4a62830937887be
Ali <m.ali.rezaeiii09@gmail.com>
2025-01-19 21:56:26 +0330

    Cleanup

--- app/src/main/java/com/example/graphql/presentation/CountryScreen.kt
+++ app/src/main/java/com/example/graphql/presentation/CountryScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
